2aa16a96a2af12f32630cbae460af8c9db361034
joshgamer474 <joshua9906@hotmail.com>
2018-04-30 22:33:16 -0400

    Updated to get the dev zip which includes headers and build .libs and .dll

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile
 from conans.tools import download, unzip
+import os
 
 class SDL(ConanFile):
 
@@ -12,19 +13,24 @@ class SDL(ConanFile):
                 "arch": ["x86", "x86_64"]}
     
     def source(self):
-        if self.settings.arch == "x86_64":
-            arch = "x64"
-        else:
-            arch = "x86"
-        
         site = "https://www.libsdl.org/release/"
-        zipName = "SDL2-%s-win32-%s.zip" % (self.version, arch)
+        zipName = "SDL2-devel-%s-VC.zip" % self.version
         download(site + zipName, zipName)
         unzip(zipName)
     
     def package(self):
-        self.copy("*.dll", dst="bin", keep_path=False)
+        if self.settings.arch == "x86_64":
+            arch = "x64"
+        else: 
+            arch = "x86"
+        folderName = "SDL2-%s" % self.version
+        self.copy("*.h",   src=folderName + "/include",       dst="include", keep_path=False)
+        self.copy("*.lib", src=folderName + "/lib/%s" % arch, dst="lib",     keep_path=False)
+        self.copy("*.dll", src=folderName + "/lib/%s" % arch, dst="bin",     keep_path=False)
+        self.copy("*.txt", src=folderName, dst="")
         
     def package_info(self):
-        self.cpp_info.bindirs = ["bin"]
+        self.cpp_info.includedirs   = ["include"]
+        self.cpp_info.libdirs       = ["lib"]
+        self.cpp_info.bindirs       = ["bin"]
     
